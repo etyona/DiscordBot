f547a1d37abb6bce70ae44453da0466ba765995a
etyona <shun.go.0218`gmail.com>
2022-02-22 16:28:11 +0900

    change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const cron = require('node-cron');
 
 // discord BOTトークン 環境変数から取得
 require('dotenv').config();
@@ -12,6 +13,45 @@ client.on('ready', () => {
     client.user.setActivity('PO!')
 });
 
+//定時処理
+cron.schedule('30 * * * *', () => {
+    const random = Math.floor( Math.random() * 10 );
+
+    switch (random) {
+        case 1:
+            console.log('良い子は寝る時間ですよ');
+            break;
+        case 2:
+            console.log('ぴゃああああああああ！');
+            break;
+        case 3:
+            console.log('おやすみなさい');
+            break;
+        case 4:
+            console.log('日付変わったってマ？');
+            break;
+        case 5:
+            console.log('Zzz...');
+            break;
+        case 6:
+            console.log('マンボ！');
+            break;
+        case 7:
+            console.log('あくてぃが24時をお知らせします。');
+            break;
+        case 8:
+            console.log('日付が変わりました。');
+            break;
+        case 9:
+            console.log('寝ろ');
+            break;
+        default:
+            console.log('おやすみ');
+            break;
+    }
+})
+
+//特定のメッセージに反応
 client.on ('messageCreate', (message) => {
     if(message.author.bot) return;
 

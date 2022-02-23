85b0d1fd631604837e082a70a39a8c1b82d8a2d9
etyona <shun.go.0218`gmail.com>
2022-02-23 16:21:32 +0900

    change

--- timeMessage.js
+++ timeMessage.js
@@ -1,42 +1,48 @@
 const cron = require('node-cron');
-const channelid = '945595621983793155';
+const channelid = '748024157442408458';
 
 function timeMessage(client){
 
     cron.schedule('0 0 * * *', () => {
-        const random = Math.floor( Math.random() * 10 );
+        const random = Math.floor( Math.random() * 10 )
         switch (random) {
             case 1:
-                client.channels.cache.get(channelid).send('良い子は寝る時間ですよ');
+                client.channels.cache.get(channelid).send('良い子は寝る時間ですよ')
                 break;
             case 2:
-                client.channels.cache.get(channelid).send('ぴゃああああああああ！');
+                client.channels.cache.get(channelid).send('ぴゃああああああああ！')
                 break;
             case 3:
-                client.channels.cache.get(channelid).send('おやすみなさい');
+                client.channels.cache.get(channelid).send('おやすみなさい')
                 break;
             case 4:
-                client.channels.cache.get(channelid).send('日付変わったってマ？');
+                client.channels.cache.get(channelid).send('日付変わったってマ？')
                 break;
             case 5:
-                client.channels.cache.get(channelid).send('Zzz...');
+                client.channels.cache.get(channelid).send('Zzz...')
                 break;
             case 6:
-                client.channels.cache.get(channelid).send('マンボ！');
+                client.channels.cache.get(channelid).send('マンボ！')
                 break;
             case 7:
-                client.channels.cache.get(channelid).send('0時をお知らせします。');
+                client.channels.cache.get(channelid).send('0時をお知らせします。')
                 break;
             case 8:
-                client.channels.cache.get(channelid).send('日付が変わりました。');
+                client.channels.cache.get(channelid).send('日付が変わりました。')
                 break;
             case 9:
-                client.channels.cache.get(channelid).send('寝ろ');
+                client.channels.cache.get(channelid).send('寝ろ')
                 break;
             default:
-                client.channels.cache.get(channelid).send('おやすみ');
+                client.channels.cache.get(channelid).send('おやすみ')
                 break;
         }
+        const today = new Date() ;
+        const oomisoka = today.getFullYear() + "/12/31";
+        const leftDays = Math.floor((Date.parse(oomisoka) - today.getTime()  ) / (24*60*60*1000)) + 1;
+        
+        // 残り日数
+        client.channels.cache.get(channelid).send('今年は残り'+leftDays+'日です')
     })
 }
 
